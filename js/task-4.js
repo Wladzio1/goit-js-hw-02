b5440645f8f27226b16cde5695e3e70936ad51c7
Wladek <vladislavfityak@gmail.com>
2025-10-21 11:07:32 +0200

    fix mistajew in 4 homework

--- js/task-4.js
+++ js/task-4.js
@@ -2,19 +2,14 @@ function getShippingCost(country) {
   switch (country) {
     case "China":
       return "Shipping to China will cost 100 credits";
-      break;
     case "Chile":
       return "Shipping to Chile will cost 250 credits";
-      break;
     case "Australia":
-      return "Shipping to Australis willc cost 170 credits";
-      break;
+      return "Shipping to Australia will cost 170 credits";
     case "Jamaica":
       return "Shipping to Jamaica will cost 120 credits";
-      break;
     default:
       return "Sorry, there is no delivery to your country";
-      break;
   }
 }
 
